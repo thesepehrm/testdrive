48b1466806a1a06ca3e7fcc625830b1b7e1a0e30
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2021-09-28 18:35:10 +0330

    implemented auto loading of chains , algorithms and challenges

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,70 @@
-export class App {
+import fs from "node:fs";
+import ChainState from "./interfaces/chainstate";
+import Algorithm from "./interfaces/algorithm";
+import chalk from "chalk";
+import print from "./util/print";
+import Challenge from "./interfaces/challenge";
+
+export default class App {
+    algorithms: {[key: string]: Algorithm} = {};
+    chains: {[key: string]: ChainState} = {};
+    challenges: {[key: string] :Challenge} = {};
+
     constructor() {
         
     }
-    public run() {
+
+    async init() {
+        let algorithmsCount = await this.loadAlgorithms()
+        let chainsCount = await this.loadChains()
+        let challengesCount = await this.loadChallenges()
+
+        print(chalk.green(`Loaded ${algorithmsCount} algorithms`))
+        print(chalk.green(`Loaded ${chainsCount} chains`))
+        print(chalk.green(`Loaded ${challengesCount} challenges`))
+
     }
+
+    run() {
+       
+        
+    }
+
+
+    async loadAlgorithms(): Promise<number> {
+        let algorithmsFiles = fs.readdirSync("./src/algorithms")
+        algorithmsFiles = algorithmsFiles.filter(p => p.endsWith(".ts"))
+        for (let p of algorithmsFiles) {
+            let algorithm: Algorithm = new (await import (`./algorithms/${p}`)).default()
+            this.algorithms[algorithm.name] = algorithm
+        }
+
+        return algorithmsFiles.length
+    }
+
+    async loadChains(): Promise<number> {
+        let chainsFiles = fs.readdirSync("./src/chains")
+        chainsFiles = chainsFiles.filter(p => p.endsWith(".ts"))
+        for (let p of chainsFiles) {
+            let chain: ChainState = new (await import (`./chains/${p}`)).default()
+            this.chains[chain.name] = chain
+        }
+
+        return chainsFiles.length
+    }
+
+    async loadChallenges():Promise<number> {
+        let challengesFiles = fs.readdirSync("./src/challenges")
+        challengesFiles = challengesFiles.filter(p => p.endsWith(".ts"))
+        for (let p of challengesFiles) {
+            let challenge: Challenge = new (await import (`./challenges/${p}`)).default()
+            this.challenges[challenge.name] = challenge
+        }
+        return challengesFiles.length
+    }
+
+    getChains(): string[] {
+        return Object.keys(this.chains)
+    }
+
 }
